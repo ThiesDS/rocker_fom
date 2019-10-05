b5108621b8f67f9bf59c7f205088026ce1bce186
Sven Thies <s.thies@traumfewo.me>
2019-10-05 12:11:21 +0200

    Separate build and run commands and remove absolute path in volume mount

--- start_container.sh
+++ start_container.sh
@@ -1,7 +1,7 @@
-docker build -t rocker_fom .
+#!/bin/bash
 
 docker run --rm -p 8787:8787 \
-	--volume /Users/sventhies/'OneDrive - Traum-Ferienwohnungen GmbH'/Privat/Sven/FOM/QFM_WS1819/docker/files/:/home/rstudio/ \
-	-e DISABLE_AUTH=true rocker_fom
+	   -v ./files/:/home/rstudio/ \
+	   -e DISABLE_AUTH=true rocker_fom
 
 
