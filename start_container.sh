e4cfc4858d9e685becbd9ad882809aef51107db3
Sven Thies <s.thies@traumfewo.me>
2019-10-05 12:20:46 +0200

    Add relative path instead of absolute one.

--- start_container.sh
+++ start_container.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Get path to current directory
+cur_path=$(PWD)
+
+# Run container with volume mount of files folder for persistent storage
 docker run --rm -p 8787:8787 \
-	   -v ./files/:/home/rstudio/ \
+	   -v "$cur_path"/files/:/home/rstudio/ \
 	   -e DISABLE_AUTH=true rocker_fom
-
-
